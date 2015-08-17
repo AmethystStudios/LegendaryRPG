07607527e1bc4b795c5fcc3a0e7c616025fca5c9
Eddyhamilton <i8aworm1time@gmail.com>
2015-08-17 14:52:25 -0500

    WolfScroll almost finished
    Need to set texture, lore, and Destroy on use yet.

--- src/main/java/eddyhamilton/legendaryrpg/item/WolfScroll.java
+++ src/main/java/eddyhamilton/legendaryrpg/item/WolfScroll.java
@@ -5,44 +5,53 @@ import java.util.List;
 import eddyhamilton.legendaryrpg.LRPGMain;
 import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityExpBottle;
 import net.minecraft.entity.passive.EntityChicken;
 import net.minecraft.entity.passive.EntityWolf;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.pathfinding.PathEntity;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
 
+public class WolfScroll extends ItemSword {
 
-
-public class WolfScroll extends ItemSword  {
-	
-	
 	private final String setInfo;
-	
-	   public WolfScroll(ToolMaterial material, String string, String string2) {
-			super(material);
-			this.setUnlocalizedName(string);
-			this.setTextureName(LRPGMain.MODID + ":" + string);
-			this.setInfo = string2;
-	   }	   
-	   
-	   public ItemStack onItemRightClick(ItemStack itemstack, World world, EntityPlayer player)
-	    {
-           EntityWolf entitywolf = new EntityWolf(player.worldObj);
-           entitywolf.isTamed();
-           entitywolf.setLocationAndAngles(player.posX, player.posY, player.posZ, player.rotationYaw, 0.0F);
-           player.worldObj.spawnEntityInWorld(entitywolf);
-		   return itemstack;
-	    }
+
+	public WolfScroll(ToolMaterial material, String string, String string2) {
+		super(material);
+		this.setUnlocalizedName(string);
+		this.setTextureName(LRPGMain.MODID + ":" + string);
+		this.setInfo = string2;
+	}
+
+	public ItemStack onItemRightClick(ItemStack itemstack, World world, EntityPlayer player) {
+		if (!world.isRemote) {
+			EntityWolf entitywolf = new EntityWolf(player.worldObj);
+			entitywolf.func_152115_b(player.getUniqueID().toString());
+			entitywolf.setTamed(true);
+			entitywolf.setPathToEntity((PathEntity) null);
+			entitywolf.setAttackTarget((EntityLivingBase) null);
+			entitywolf.setHealth(20.0F);
+			entitywolf.worldObj.setEntityState(entitywolf, (byte) 7);
+			entitywolf.setCollarColor(10);
+			entitywolf.setPositionAndUpdate(player.posX, player.posY, player.posZ);
+			player.worldObj.spawnEntityInWorld(entitywolf);
+		}
+		return itemstack;
+	}
+
 	@Override
 	public CreativeTabs[] getCreativeTabs() {
-		return new CreativeTabs[] { LRPGMain.tabLegendaryRPG, }; 
+		return new CreativeTabs[] { LRPGMain.tabLegendaryRPG, };
 	}
 }
