4c53485842b2b90c2708c7461d005b3af00931a4
Eddyhamilton <i8aworm1time@gmail.com>
2015-12-07 23:14:58 -0600

    MESSED WITH ENCHANTMENTS

--- src/main/java/eddyhamilton/legendaryrpg/MainEventHandler.java
+++ src/main/java/eddyhamilton/legendaryrpg/MainEventHandler.java
@@ -1,5 +1,9 @@
 package eddyhamilton.legendaryrpg;
 
+import java.util.TimerTask;
+
+import com.oracle.jrockit.jfr.TimedEvent;
+
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
 import cpw.mods.fml.common.gameevent.TickEvent.PlayerTickEvent;
@@ -11,20 +15,35 @@ import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
+
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.world.WorldEvent;
 
 public class MainEventHandler {
 	
 	@SubscribeEvent
 	public void lotgtEnchantmentCheck(cpw.mods.fml.common.gameevent.PlayerEvent.ItemPickupEvent event){
 		EntityPlayer player = Minecraft.getMinecraft().thePlayer;
-		ItemStack stack = player.getHeldItem();
-    	player.addChatMessage(
-				new ChatComponentText(EnumChatFormatting.ITALIC + "HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII"));
+
+		for(int i =0;i<9;i++){
+			ItemStack stack = player.inventory.getStackInSlot(i);
     	if (stack != null && EnchantmentHelper.getEnchantmentLevel(LRPGMain.LifeoftheGreatTree.effectId, stack) > 0){
     		player.addPotionEffect(new PotionEffect(Potion.jump.id, 3000000, 3));
-    		return;
+    		player.addChatMessage(
+					new ChatComponentText(EnumChatFormatting.ITALIC + "You can repair your item here.."));
+    		int counter =100;
+    		counter--;
+    		if(counter == 0){
+        		stack.setItemDamage(stack.getItemDamage()+100000);
+        		player.addChatMessage(
+    					new ChatComponentText(EnumChatFormatting.ITALIC + "5 second delay?"));
+
+    		counter=20;
+    		}
+
     	}
+		}
 	}
 
 }
